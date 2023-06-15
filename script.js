4a435e69083527642ef3e171e2f6e6b965716394
Amal S <amalsunu5469@gmail.com>
2023-06-15 17:36:39 +0530

    Quiz first commit after completion

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const correctAnswer = ["A", "A", "C", "B", "D"];
+
+const form = document.querySelector(".quiz-form");
+
+const result = document.querySelector(".result")
+
+const button = document.getElementById('myButton')
+
+const questions = document.querySelectorAll(".question")
+
+
+button.onclick = function(){
+    location.reload();
+}
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    let score = 0;
+    
+    //value capture
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+    userAnswers.forEach((answer,index) => {
+        if(answer === correctAnswer[index]){
+            score +=1;
+            questions[index].classList.add("correct")
+
+        }else{
+            questions[index].classList.add("wrong")
+        }
+    })
+    console.log(score);
+
+    scrollTo(0, 0);
+
+    result.classList.remove("hide");
+    result.querySelector("p").textContent = `You Scored ${score}/5`;
+
+});
